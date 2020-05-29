2094f3e35a17356eee9a0e2ba387fbd81d53f7c0
Zxilly <zhouxinyu1001@gmail.com>
2020-05-29 14:32:59 +0800

    handle physical experiment

--- function.py
+++ function.py
@@ -14,6 +14,7 @@ def rmzero(input):
 
 
 def getcalender(course,server_status=0):
+    #global dtstart_time, dtend_time
     calt = icalendar.Calendar()
     calt['version'] = '2.0'
     calt['prodid'] = '-//Zxilly//JGSUCalender//CN'
@@ -47,64 +48,133 @@ def getcalender(course,server_status=0):
     }
 
     for one_course in course:
-        print(one_course)
+        if one_course['kcmc'] == '大学物理实验A1':
+            print(one_course)
 
-        week_start = int(one_course['kkzc'].split('-')[0])  # 周次开始
-        try:
-            week_end = int(one_course['kkzc'].split('-')[1])  # 周次结束
-        except IndexError:
-            week_end=week_start #catch单周课程
-        course_weekday = int(re.match(r'\d', one_course['kcsj'])[0])
+            week_start = int(one_course['kkzc'].split('-')[0])  # 周次开始
+            try:
+                week_end = int(one_course['kkzc'].split('-')[1])  # 周次结束
+            except IndexError:
+                week_end = week_start  # catch单周课程
+            course_weekday = int(re.match(r'\d', one_course['kcsj'])[0])
 
-        course_period_regrx = re.compile(r'(\d\d)')  # 编译正则
-        course_period_list = course_period_regrx.findall(one_course['kcsj'], 1)  # 得到一个课时的list
-        course_period_num = len(course_period_list)  # 一共几节课
-        course_period_start = int(course_period_list[0])  # 第一节课的课时号
-        course_period_end = int(course_period_list[course_period_num - 1])  # 最后一节课的课时号
+            course_period_regrx = re.compile(r'(\d\d)')  # 编译正则
+            course_period_list = course_period_regrx.findall(one_course['kcsj'], 1)  # 得到一个课时的list
+            course_period_num = len(course_period_list)  # 一共几节课
+            course_period_start = int(course_period_list[0])  # 第一节课的课时号
+            if course_period_start == 5:
+                dtstart_time = time(13,30)
+                dtend_time = time(15,10)
+            else:
+                dtstart_time = time(15,30)
+                dtend_time = time(17,10)
+            #else:
+            #    exit(404)
 
-        # print(course_period_list)
-        # course_time_regrx = re.search(r'(?<=\d)(\d)*','10102').group(0)
-        # course_time_list = re.split(r'(\d\d)',course_time_regrx)
+            course_period_end = int(course_period_list[course_period_num - 1])  # 最后一节课的课时号
 
-        lesson_time = relativedelta(minutes=45)
+            # print(course_period_list)
+            # course_time_regrx = re.search(r'(?<=\d)(\d)*','10102').group(0)
+            # course_time_list = re.split(r'(\d\d)',course_time_regrx)
 
-        # print(one_course['kcsj'])
+            #lesson_time = relativedelta(minutes=45)
 
-        # print(one_course)
+            # print(one_course['kcsj'])
 
-        dt_date = info.semester_start + relativedelta(weeks=(week_start - 1)) + relativedelta(
-            days=(course_weekday - 1))  # 课程日期
-        # dtstart_day = dtstart_day+
+            # print(one_course)
 
-        dtstart_time = dirt_week[course_period_start]  # 上课时间
-        dtend_time = dirt_week[course_period_end]  # 最后一节小课下课时间
+            dt_date = info.semester_start + relativedelta(weeks=(week_start - 1)) + relativedelta(
+                days=(course_weekday - 1))  # 课程日期
+            # dtstart_day = dtstart_day+
 
-        dtstart_datetime = datetime.combine(dt_date, dtstart_time, tzinfo=pytz.timezone("Asia/Shanghai"))  # 上课日期时间
+            #dtstart_time = dirt_week[course_period_start]  # 上课时间
+            #dtend_time = dirt_week[course_period_end]  # 最后一节小课下课时间
 
-        dtend_datetime = datetime.combine(dt_date, dtend_time, tzinfo=pytz.timezone("Asia/Shanghai"))  # 下课日期时间
-        dtend_datetime += lesson_time
+            dtstart_datetime = datetime.combine(dt_date, dtstart_time, tzinfo=pytz.timezone("Asia/Shanghai"))  # 上课日期时间
 
-        # dtend_datetime = dtstart_datetime + lesson_time
+            dtend_datetime = datetime.combine(dt_date, dtend_time, tzinfo=pytz.timezone("Asia/Shanghai"))  # 下课日期时间
+            #dtend_datetime += lesson_time
 
-        interval = 1 if one_course['sjbz'] == '0' else 2  # 单双周判定
+            # dtend_datetime = dtstart_datetime + lesson_time
 
-        count = (week_end - week_start + 1) if one_course['sjbz'] == '0' else (((week_end - week_start) / 2) + 1)
+            interval = 1 if one_course['sjbz'] == '0' else 2  # 单双周判定
 
-        event = icalendar.Event()
-        event.add('summary', one_course['kcmc'])  # 标题
-        event.add('uid', str(uuid1()) + '@JGSU')  # UUID
-        event.add('dtstamp', datetime.now())  # 创建时间
-        event.add('location', one_course['jsxm'] + '@' + one_course['jsmc'])  # 地点
-        event.add('description',
-                  '第{}-{}节\r\n教师： {}\r\n教室: {}'.format(course_period_start, course_period_end, one_course['jsxm'],
-                                                       one_course['jsmc']))  # 教师名称
-        event.add('dtstart', dtstart_datetime)
-        event.add('dtend', dtend_datetime)
-        event.add('rrule', {'freq': 'weekly', 'interval': interval, 'count': count})
+            count = (week_end - week_start + 1) if one_course['sjbz'] == '0' else (((week_end - week_start) / 2) + 1)
 
-        calt.add_component(event)
+            event = icalendar.Event()
+            event.add('summary', one_course['kcmc'])  # 标题
+            event.add('uid', str(uuid1()) + '@JGSU')  # UUID
+            event.add('dtstamp', datetime.now())  # 创建时间
+            event.add('location', one_course['jsxm'] + '@' + one_course['jsmc'])  # 地点
+            event.add('description',
+                      '第{}-{}节\r\n教师： {}\r\n教室: {}'.format(course_period_start, course_period_end, one_course['jsxm'],
+                                                           one_course['jsmc']))  # 教师名称
+            event.add('dtstart', dtstart_datetime)
+            event.add('dtend', dtend_datetime)
+            event.add('rrule', {'freq': 'weekly', 'interval': interval, 'count': count})
 
-        # print_cal(calt)
+            calt.add_component(event)
+
+            # print_cal(calt)
+        else:
+            print(one_course)
+
+            week_start = int(one_course['kkzc'].split('-')[0])  # 周次开始
+            try:
+                week_end = int(one_course['kkzc'].split('-')[1])  # 周次结束
+            except IndexError:
+                week_end = week_start  # catch单周课程
+            course_weekday = int(re.match(r'\d', one_course['kcsj'])[0])
+
+            course_period_regrx = re.compile(r'(\d\d)')  # 编译正则
+            course_period_list = course_period_regrx.findall(one_course['kcsj'], 1)  # 得到一个课时的list
+            course_period_num = len(course_period_list)  # 一共几节课
+            course_period_start = int(course_period_list[0])  # 第一节课的课时号
+            course_period_end = int(course_period_list[course_period_num - 1])  # 最后一节课的课时号
+
+            # print(course_period_list)
+            # course_time_regrx = re.search(r'(?<=\d)(\d)*','10102').group(0)
+            # course_time_list = re.split(r'(\d\d)',course_time_regrx)
+
+            lesson_time = relativedelta(minutes=45)
+
+            # print(one_course['kcsj'])
+
+            # print(one_course)
+
+            dt_date = info.semester_start + relativedelta(weeks=(week_start - 1)) + relativedelta(
+                days=(course_weekday - 1))  # 课程日期
+            # dtstart_day = dtstart_day+
+
+            dtstart_time = dirt_week[course_period_start]  # 上课时间
+            dtend_time = dirt_week[course_period_end]  # 最后一节小课下课时间
+
+            dtstart_datetime = datetime.combine(dt_date, dtstart_time, tzinfo=pytz.timezone("Asia/Shanghai"))  # 上课日期时间
+
+            dtend_datetime = datetime.combine(dt_date, dtend_time, tzinfo=pytz.timezone("Asia/Shanghai"))  # 下课日期时间
+            dtend_datetime += lesson_time
+
+            # dtend_datetime = dtstart_datetime + lesson_time
+
+            interval = 1 if one_course['sjbz'] == '0' else 2  # 单双周判定
+
+            count = (week_end - week_start + 1) if one_course['sjbz'] == '0' else (((week_end - week_start) / 2) + 1)
+
+            event = icalendar.Event()
+            event.add('summary', one_course['kcmc'])  # 标题
+            event.add('uid', str(uuid1()) + '@JGSU')  # UUID
+            event.add('dtstamp', datetime.now())  # 创建时间
+            event.add('location', one_course['jsxm'] + '@' + one_course['jsmc'])  # 地点
+            event.add('description',
+                      '第{}-{}节\r\n教师： {}\r\n教室: {}'.format(course_period_start, course_period_end, one_course['jsxm'],
+                                                           one_course['jsmc']))  # 教师名称
+            event.add('dtstart', dtstart_datetime)
+            event.add('dtend', dtend_datetime)
+            event.add('rrule', {'freq': 'weekly', 'interval': interval, 'count': count})
+
+            calt.add_component(event)
+
+            # print_cal(calt)
 
     if server_status==0:
         with open('output.ics', 'w+', encoding='utf-8', newline='') as file:
