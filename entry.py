2d9bec95b792bd033146da1701ca44601d7141bd
Zxilly <zhouxinyu1001@gmail.com>
2020-08-12 12:27:49 +0800

    backup

--- entry.py
+++ entry.py
@@ -1,6 +1,7 @@
 import base64
 import hashlib
 import json
+import re
 from datetime import datetime
 
 import requests
@@ -15,7 +16,6 @@ header = {
     'csrfToken': hashlib.md5((str(int(datetime.now().timestamp())) + "lyedu").encode('UTF-8')).hexdigest()
     # 'csrfToken':'610453b7b20b8a4402042a201b3665b1',
 }
-
 
 if __name__ == '__main__':
     mainSession = requests.session()
@@ -34,6 +34,8 @@ if __name__ == '__main__':
     }
 
     req = mainSession.post(url=api.login, params=loginDict, data={})
+
+    print(req.json())
 
     userData = json.loads(req.cookies.get_dict()['user'])
 
@@ -57,14 +59,60 @@ if __name__ == '__main__':
     tmpData = req['data']
 
     courseData = {}
+    parsedCourseData = {}
 
     for course in tmpData:
         time = course['time']['timeCode']
         week = course['week']['weekCode']
-        key = int(week)*100+int(time)
-        courseData[key]=course['courseList']
+        key = int(week) * 100 + int(time)
+        courseData[key]={}
+        for course in course['courseList']:
+            courseData[key][course['courseCode']] = course
 
-    print(json.dumps(courseData,ensure_ascii=False))
+    parsedCourseDataPointer = 0
+    for weekPointer in range(1, 8):
+        for coursePointer in range(1, 12):
+            print(weekPointer * 100 + coursePointer)
+            if courseData[weekPointer * 100 + coursePointer]:
 
 
 
+
+
+
+    for weekPointer in range(1, 8):
+        parsedCourseDataPointer = 0
+        for coursePointer in range(1, 12):
+            print(weekPointer * 100 + coursePointer)
+            if courseData[weekPointer * 100 + coursePointer]:
+                data = courseData[weekPointer * 100 + coursePointer]
+                if parsedCourseData:
+                    if parsedCourseData[parsedCourseDataPointer]:
+                        if data['courseName'] == parsedCourseData[parsedCourseDataPointer]['courseName']:
+                            parsedCourseData[parsedCourseDataPointer]['endTime'] = coursePointer
+                            continue
+                        else:
+                            parsedCourseDataPointer += 1
+                reObject = re.compile(r'(\d*)-(\d*)( 单)?')
+                print(data['weeks'])
+                parsedWeek = re.match(reObject, data['weeks']).groups()
+                startWeek = parsedWeek[0]
+                endWeek = parsedWeek[1]
+                interval = 2 if parsedWeek[2] else 1
+                parsedCourseData[parsedCourseDataPointer] = {
+                    "courseName": data['courseName'],
+                    'teacherName': data['teacherName'],
+                    'className': data['className'],
+                    'classroomName': data['classroomName'],
+                    'numberOfStudent': data['numberOfStudent'],
+                    'startTime': coursePointer,
+                    'endTime': coursePointer,
+                    'startWeek': startWeek,
+                    'endWeek': endTime,
+                    'interval': interval,
+                }
+        if parsedCourseData:
+            if not parsedCourseData[parsedCourseDataPointer]:
+                parsedCourseDataPointer += 1
+
+    print(parsedCourseData)
