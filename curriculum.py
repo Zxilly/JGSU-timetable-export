60792fc9849db968eb4cbefc2636c67b50f12d87
Zxilly <zxilly@outlook.com>
2022-05-10 16:32:14 +0800

    refactor: rename vars
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- curriculum.py
+++ curriculum.py
@@ -19,131 +19,130 @@ header = {
 
 def curriculum(cookies: str = None):
     print(cookies)
-    mainSession, userID, student_num, semesterName, semesterStartTime = login(cookies)
+    main_session, user_id, student_num, semester_name, semester_start_time = login(cookies)
 
-    req = mainSession.post(url=api.course, json={
+    req = main_session.post(url=api.course, json={
         "oddOrDouble": 0,
-        "semester": semesterName,
+        "semester": semester_name,
         "startWeek": "1",
         "stopWeek": "22",
-        "studentId": userID,
+        "studentId": user_id,
         "weeks": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
     }).json()
 
-    print(mainSession.cookies.items())
+    print(main_session.cookies.items())
     print(req)
-    tmpData = req['data']
-    reObject = re.compile(r'(\d*)-(\d*)( [单双])?')
-    allCourseData = []
+    tmp_data = req['data']
+    all_course_data = []
 
-    for timeSection in tmpData:
+    for timeSection in tmp_data:
         for timeSectionCourse in timeSection['courseList']:
-            allCourseData.append(timeSectionCourse)
+            all_course_data.append(timeSectionCourse)
 
-    purgeAllCourseData = {}
+    purge_all_course_data = {}
 
     for day in range(1, 8):
         for time in range(1, 12):
-            timeSign = (day, time)
-            purgeAllCourseData[timeSign] = {}
+            time_sign = (day, time)
+            purge_all_course_data[time_sign] = {}
 
-    for courseData in allCourseData:
-        day = fix_day(int(courseData['dayOfWeek']))
-        time = int(courseData['time'])
-        timeSign = (day, time)
-        purgeCourseDict = {
-            'courseName': courseData['courseName'],
-            'weeks': courseData['weeks'],
-            'teacher': courseData.get('teacherName', ''),
-            'studentNum': courseData['numberOfStudent'],
-            'classroomName': courseData.get('classroomName', ''),
-            'className': courseData.get('teachingClassName', '')
+    for course_data in all_course_data:
+        day = fix_day(int(course_data['dayOfWeek']))
+        time = int(course_data['time'])
+        time_sign = (day, time)
+        purge_course_dict = {
+            'courseName': course_data['courseName'],
+            'weeks': course_data['weeks'],
+            'teacher': course_data.get('teacherName', ''),
+            'studentNum': course_data['numberOfStudent'],
+            'classroomName': course_data.get('classroomName', ''),
+            'className': course_data.get('teachingClassName', '')
         }
-        purgeAllCourseData.setdefault(timeSign, {})
-        purgeAllCourseData[timeSign][dict_hash(purgeCourseDict)] = purgeCourseDict
+        purge_all_course_data.setdefault(time_sign, {})
+        purge_all_course_data[time_sign][dict_hash(purge_course_dict)] = purge_course_dict
 
-    parsedCourseData = []
+    parsed_course_data = []
 
-    for timeSign in purgeAllCourseData.keys():
-        day = timeSign[0]
-        time = timeSign[1]
+    for time_sign in purge_all_course_data.keys():
+        day = time_sign[0]
+        time = time_sign[1]
         while True:
-            if purgeAllCourseData[timeSign]:
-                currentOptCourse = purgeAllCourseData[timeSign].popitem()
+            if purge_all_course_data[time_sign]:
+                current_opt_course = purge_all_course_data[time_sign].popitem()
 
-                courseHash = currentOptCourse[0]
-                courseData = currentOptCourse[1]
+                course_hash = current_opt_course[0]
+                course_data = current_opt_course[1]
 
-                courseName = courseData['courseName']
+                course_name = course_data['courseName']
 
-                teacher = courseData['teacher']
-                studentNum = courseData['studentNum']
-                className = courseData['className']
-                classroomName = courseData['classroomName']
+                teacher = course_data['teacher']
+                student_num = course_data['studentNum']
+                class_name = course_data['className']
+                classroom_name = course_data['classroomName']
 
-                rawWeeks = courseData['weeks']
+                raw_weeks = course_data['weeks']
                 try:
-                    parsedWeeks = re.match(reObject, rawWeeks).groups()
-                    interval = 2 if parsedWeeks[2] else 1
-                    startWeek = parsedWeeks[0]
-                    endWeek = parsedWeeks[1]
+                    parsed_weeks = re.match(re_object, raw_weeks).groups()
+                    interval = 2 if parsed_weeks[2] else 1
+                    start_week = parsed_weeks[0]
+                    end_week = parsed_weeks[1]
                 except AttributeError:
-                    parsedWeeks = int(rawWeeks)
+                    parsed_weeks = int(raw_weeks)
                     interval = 1
-                    startWeek = parsedWeeks
-                    endWeek = parsedWeeks
+                    start_week = parsed_weeks
+                    end_week = parsed_weeks
 
-                startTimePointer = copy.copy(time)
-                endTimePointer = copy.copy(time)
+                start_time_pointer = copy.copy(time)
+                end_time_pointer = copy.copy(time)
 
                 # print(courseName, str(parsedWeeks), rawWeeks)
                 while True:
-                    if endTimePointer != 4 and endTimePointer != 11:
-                        if courseHash in purgeAllCourseData[(day, endTimePointer + 1)].keys():
-                            purgeAllCourseData[(day, endTimePointer + 1)].pop(courseHash)
-                            endTimePointer += 1
+                    if end_time_pointer != 4 and end_time_pointer != 11:
+                        if course_hash in purge_all_course_data[(day, end_time_pointer + 1)].keys():
+                            purge_all_course_data[(day, end_time_pointer + 1)].pop(course_hash)
+                            end_time_pointer += 1
                         else:
                             break
                     else:
                         break
 
-                parsedOneCourse = {
+                parsed_one_course = {
                     'day': day,
-                    'courseName': courseName,
-                    'className': className,
-                    'classroomName': classroomName,
-                    'startTimeID': startTimePointer,
-                    'endTimeID': endTimePointer,
+                    'courseName': course_name,
+                    'className': class_name,
+                    'classroomName': classroom_name,
+                    'startTimeID': start_time_pointer,
+                    'endTimeID': end_time_pointer,
                     'teacherName': teacher,
-                    'startWeek': startWeek,
-                    'endWeek': endWeek,
+                    'startWeek': start_week,
+                    'endWeek': end_week,
                     'interval': interval,
-                    'studentNumber': studentNum
+                    'studentNumber': student_num
                 }
 
-                oneCourseHash = dict_hash(parsedOneCourse)
-                print(oneCourseHash, parsedOneCourse)
-                if oneCourseHash in fix_dict.keys():
-                    fix_object = fix_dict[oneCourseHash]
+                one_course_hash = dict_hash(parsed_one_course)
+                print(one_course_hash, parsed_one_course)
+                if one_course_hash in fix_dict.keys():
+                    fix_object = fix_dict[one_course_hash]
                     for key, value in fix_object.items():
-                        parsedOneCourse[key] = value
-                    print("fixed:", parsedOneCourse, file=sys.stderr)
+                        parsed_one_course[key] = value
+                    print("fixed:", parsed_one_course, file=sys.stderr)
 
-                parsedOneCourse['startTime'] = courseTimeDict[parsedOneCourse['startTimeID']]
-                parsedOneCourse['endTime'] = courseTimeDict[parsedOneCourse['endTimeID']] + COURSE_TIME
+                parsed_one_course['startTime'] = courseTimeDict[parsed_one_course['startTimeID']]
+                parsed_one_course['endTime'] = courseTimeDict[parsed_one_course['endTimeID']] + COURSE_TIME
 
-                parsedCourseData.append(copy.deepcopy(parsedOneCourse))
-                parsedOneCourse.clear()
+                parsed_course_data.append(copy.deepcopy(parsed_one_course))
+                parsed_one_course.clear()
             else:
                 break
 
-    calt = get_ical(f'{semesterName} 课表')
+    calt = get_ical(f'{semester_name} 课表')
 
-    for oneEvent in parsedCourseData:
+    for oneEvent in parsed_course_data:
         count = int((int(oneEvent['endWeek']) - int(oneEvent['startWeek'])) / int(oneEvent['interval']) + 1)
-        dtstart_datetime = semesterStartTime + (int(oneEvent['startWeek']) - 1) * ONE_WEEK + (
+        dtstart_datetime = semester_start_time + (int(oneEvent['startWeek']) - 1) * ONE_WEEK + (
                 int(oneEvent['day']) - 1) * ONE_DAY + oneEvent['startTime']
-        dtend_datetime = semesterStartTime + (int(oneEvent['startWeek']) - 1) * ONE_WEEK + (
+        dtend_datetime = semester_start_time + (int(oneEvent['startWeek']) - 1) * ONE_WEEK + (
                 int(oneEvent['day']) - 1) * ONE_DAY + oneEvent['endTime']
 
         event = icalendar.Event()
@@ -179,10 +178,10 @@ def curriculum(cookies: str = None):
             event.add('rrule', {'freq': 'weekly', 'interval': oneEvent['interval'], 'count': count})
         calt.add_component(event)
 
-    with open(f'data/{student_num}.{semesterName}.curriculum.ics', 'wb') as f:
+    with open(f'data/{student_num}.{semester_name}.curriculum.ics', 'wb') as f:
         f.write(calt.to_ical())
 
-    return f'https://ical.learningman.top/{student_num}/{semesterName}/curriculum.ics'
+    return f'https://ical.learningman.top/{student_num}/{semester_name}/curriculum.ics'
 
 
 if __name__ == '__main__':
