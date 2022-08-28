7c6cf25180ee18dd931fa43795e511e740c170a9
Zxilly <zxilly@outlook.com>
2022-08-28 23:15:18 +0800

    fix: wrap async
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- sendCurPayload.js
+++ sendCurPayload.js
@@ -1,187 +1,190 @@
-function md5(inputString) {
-    const hc = "0123456789abcdef";
+(async () => {
+    function md5(inputString) {
+        const hc = "0123456789abcdef";
 
-    function rh(n) {
-        let j, s = "";
-        for (j = 0; j <= 3; j++) s += hc.charAt((n >> (j * 8 + 4)) & 0x0F) + hc.charAt((n >> (j * 8)) & 0x0F);
-        return s;
+        function rh(n) {
+            let j, s = "";
+            for (j = 0; j <= 3; j++) s += hc.charAt((n >> (j * 8 + 4)) & 0x0F) + hc.charAt((n >> (j * 8)) & 0x0F);
+            return s;
+        }
+
+        function ad(x, y) {
+            const l = (x & 0xFFFF) + (y & 0xFFFF);
+            const m = (x >> 16) + (y >> 16) + (l >> 16);
+            return (m << 16) | (l & 0xFFFF);
+        }
+
+        function rl(n, c) {
+            return (n << c) | (n >>> (32 - c));
+        }
+
+        function cm(q, a, b, x, s, t) {
+            return ad(rl(ad(ad(a, q), ad(x, t)), s), b);
+        }
+
+        function ff(a, b, c, d, x, s, t) {
+            return cm((b & c) | ((~b) & d), a, b, x, s, t);
+        }
+
+        function gg(a, b, c, d, x, s, t) {
+            return cm((b & d) | (c & (~d)), a, b, x, s, t);
+        }
+
+        function hh(a, b, c, d, x, s, t) {
+            return cm(b ^ c ^ d, a, b, x, s, t);
+        }
+
+        function ii(a, b, c, d, x, s, t) {
+            return cm(c ^ (b | (~d)), a, b, x, s, t);
+        }
+
+        function sb(x) {
+            let i;
+            const nblk = ((x.length + 8) >> 6) + 1;
+            const blks = new Array(nblk * 16);
+            for (i = 0; i < nblk * 16; i++) blks[i] = 0;
+            for (i = 0; i < x.length; i++) blks[i >> 2] |= x.charCodeAt(i) << ((i % 4) * 8);
+            blks[i >> 2] |= 0x80 << ((i % 4) * 8);
+            blks[nblk * 16 - 2] = x.length * 8;
+            return blks;
+        }
+
+        let i, x = sb(inputString), a = 1732584193, b = -271733879, c = -1732584194, d = 271733878, olda, oldb, oldc,
+            oldd;
+        for (i = 0; i < x.length; i += 16) {
+            olda = a;
+            oldb = b;
+            oldc = c;
+            oldd = d;
+            a = ff(a, b, c, d, x[i], 7, -680876936);
+            d = ff(d, a, b, c, x[i + 1], 12, -389564586);
+            c = ff(c, d, a, b, x[i + 2], 17, 606105819);
+            b = ff(b, c, d, a, x[i + 3], 22, -1044525330);
+            a = ff(a, b, c, d, x[i + 4], 7, -176418897);
+            d = ff(d, a, b, c, x[i + 5], 12, 1200080426);
+            c = ff(c, d, a, b, x[i + 6], 17, -1473231341);
+            b = ff(b, c, d, a, x[i + 7], 22, -45705983);
+            a = ff(a, b, c, d, x[i + 8], 7, 1770035416);
+            d = ff(d, a, b, c, x[i + 9], 12, -1958414417);
+            c = ff(c, d, a, b, x[i + 10], 17, -42063);
+            b = ff(b, c, d, a, x[i + 11], 22, -1990404162);
+            a = ff(a, b, c, d, x[i + 12], 7, 1804603682);
+            d = ff(d, a, b, c, x[i + 13], 12, -40341101);
+            c = ff(c, d, a, b, x[i + 14], 17, -1502002290);
+            b = ff(b, c, d, a, x[i + 15], 22, 1236535329);
+            a = gg(a, b, c, d, x[i + 1], 5, -165796510);
+            d = gg(d, a, b, c, x[i + 6], 9, -1069501632);
+            c = gg(c, d, a, b, x[i + 11], 14, 643717713);
+            b = gg(b, c, d, a, x[i], 20, -373897302);
+            a = gg(a, b, c, d, x[i + 5], 5, -701558691);
+            d = gg(d, a, b, c, x[i + 10], 9, 38016083);
+            c = gg(c, d, a, b, x[i + 15], 14, -660478335);
+            b = gg(b, c, d, a, x[i + 4], 20, -405537848);
+            a = gg(a, b, c, d, x[i + 9], 5, 568446438);
+            d = gg(d, a, b, c, x[i + 14], 9, -1019803690);
+            c = gg(c, d, a, b, x[i + 3], 14, -187363961);
+            b = gg(b, c, d, a, x[i + 8], 20, 1163531501);
+            a = gg(a, b, c, d, x[i + 13], 5, -1444681467);
+            d = gg(d, a, b, c, x[i + 2], 9, -51403784);
+            c = gg(c, d, a, b, x[i + 7], 14, 1735328473);
+            b = gg(b, c, d, a, x[i + 12], 20, -1926607734);
+            a = hh(a, b, c, d, x[i + 5], 4, -378558);
+            d = hh(d, a, b, c, x[i + 8], 11, -2022574463);
+            c = hh(c, d, a, b, x[i + 11], 16, 1839030562);
+            b = hh(b, c, d, a, x[i + 14], 23, -35309556);
+            a = hh(a, b, c, d, x[i + 1], 4, -1530992060);
+            d = hh(d, a, b, c, x[i + 4], 11, 1272893353);
+            c = hh(c, d, a, b, x[i + 7], 16, -155497632);
+            b = hh(b, c, d, a, x[i + 10], 23, -1094730640);
+            a = hh(a, b, c, d, x[i + 13], 4, 681279174);
+            d = hh(d, a, b, c, x[i], 11, -358537222);
+            c = hh(c, d, a, b, x[i + 3], 16, -722521979);
+            b = hh(b, c, d, a, x[i + 6], 23, 76029189);
+            a = hh(a, b, c, d, x[i + 9], 4, -640364487);
+            d = hh(d, a, b, c, x[i + 12], 11, -421815835);
+            c = hh(c, d, a, b, x[i + 15], 16, 530742520);
+            b = hh(b, c, d, a, x[i + 2], 23, -995338651);
+            a = ii(a, b, c, d, x[i], 6, -198630844);
+            d = ii(d, a, b, c, x[i + 7], 10, 1126891415);
+            c = ii(c, d, a, b, x[i + 14], 15, -1416354905);
+            b = ii(b, c, d, a, x[i + 5], 21, -57434055);
+            a = ii(a, b, c, d, x[i + 12], 6, 1700485571);
+            d = ii(d, a, b, c, x[i + 3], 10, -1894986606);
+            c = ii(c, d, a, b, x[i + 10], 15, -1051523);
+            b = ii(b, c, d, a, x[i + 1], 21, -2054922799);
+            a = ii(a, b, c, d, x[i + 8], 6, 1873313359);
+            d = ii(d, a, b, c, x[i + 15], 10, -30611744);
+            c = ii(c, d, a, b, x[i + 6], 15, -1560198380);
+            b = ii(b, c, d, a, x[i + 13], 21, 1309151649);
+            a = ii(a, b, c, d, x[i + 4], 6, -145523070);
+            d = ii(d, a, b, c, x[i + 11], 10, -1120210379);
+            c = ii(c, d, a, b, x[i + 2], 15, 718787259);
+            b = ii(b, c, d, a, x[i + 9], 21, -343485551);
+            a = ad(a, olda);
+            b = ad(b, oldb);
+            c = ad(c, oldc);
+            d = ad(d, oldd);
+        }
+        return rh(a) + rh(b) + rh(c) + rh(d);
     }
 
-    function ad(x, y) {
-        const l = (x & 0xFFFF) + (y & 0xFFFF);
-        const m = (x >> 16) + (y >> 16) + (l >> 16);
-        return (m << 16) | (l & 0xFFFF);
+    function getCSRFToken() {
+        return md5(Date.now().toString() + "lyedu");
     }
 
-    function rl(n, c) {
-        return (n << c) | (n >>> (32 - c));
-    }
+    const semester_resp = await fetch("/api/baseInfo/semester/selectCurrentXnXq", {
+        headers: {
+            "accept": "application/json, text/plain, */*",
+            "authorization": "Bearer undefined",
+            "csrftoken": getCSRFToken(),
+        },
+        method: "GET",
+    });
+    const semester_data = await semester_resp.json();
 
-    function cm(q, a, b, x, s, t) {
-        return ad(rl(ad(ad(a, q), ad(x, t)), s), b);
-    }
+    const user = JSON.parse(('; ' + document.cookie).split(`; user=`).pop().split(';')[0]);
+    const user_id = user['userName'];
 
-    function ff(a, b, c, d, x, s, t) {
-        return cm((b & c) | ((~b) & d), a, b, x, s, t);
-    }
+    const num_resp = await fetch(`https://jw.jgsu.edu.cn:19995/api/student/studentInfo/selectXsSyByid/${user_id}`);
+    const num_data = await num_resp.json();
+    const student_num = num_data['data']['xh'];
 
-    function gg(a, b, c, d, x, s, t) {
-        return cm((b & d) | (c & (~d)), a, b, x, s, t);
-    }
-
-    function hh(a, b, c, d, x, s, t) {
-        return cm(b ^ c ^ d, a, b, x, s, t);
-    }
-
-    function ii(a, b, c, d, x, s, t) {
-        return cm(c ^ (b | (~d)), a, b, x, s, t);
-    }
-
-    function sb(x) {
-        let i;
-        const nblk = ((x.length + 8) >> 6) + 1;
-        const blks = new Array(nblk * 16);
-        for (i = 0; i < nblk * 16; i++) blks[i] = 0;
-        for (i = 0; i < x.length; i++) blks[i >> 2] |= x.charCodeAt(i) << ((i % 4) * 8);
-        blks[i >> 2] |= 0x80 << ((i % 4) * 8);
-        blks[nblk * 16 - 2] = x.length * 8;
-        return blks;
-    }
-
-    let i, x = sb(inputString), a = 1732584193, b = -271733879, c = -1732584194, d = 271733878, olda, oldb, oldc, oldd;
-    for (i = 0; i < x.length; i += 16) {
-        olda = a;
-        oldb = b;
-        oldc = c;
-        oldd = d;
-        a = ff(a, b, c, d, x[i], 7, -680876936);
-        d = ff(d, a, b, c, x[i + 1], 12, -389564586);
-        c = ff(c, d, a, b, x[i + 2], 17, 606105819);
-        b = ff(b, c, d, a, x[i + 3], 22, -1044525330);
-        a = ff(a, b, c, d, x[i + 4], 7, -176418897);
-        d = ff(d, a, b, c, x[i + 5], 12, 1200080426);
-        c = ff(c, d, a, b, x[i + 6], 17, -1473231341);
-        b = ff(b, c, d, a, x[i + 7], 22, -45705983);
-        a = ff(a, b, c, d, x[i + 8], 7, 1770035416);
-        d = ff(d, a, b, c, x[i + 9], 12, -1958414417);
-        c = ff(c, d, a, b, x[i + 10], 17, -42063);
-        b = ff(b, c, d, a, x[i + 11], 22, -1990404162);
-        a = ff(a, b, c, d, x[i + 12], 7, 1804603682);
-        d = ff(d, a, b, c, x[i + 13], 12, -40341101);
-        c = ff(c, d, a, b, x[i + 14], 17, -1502002290);
-        b = ff(b, c, d, a, x[i + 15], 22, 1236535329);
-        a = gg(a, b, c, d, x[i + 1], 5, -165796510);
-        d = gg(d, a, b, c, x[i + 6], 9, -1069501632);
-        c = gg(c, d, a, b, x[i + 11], 14, 643717713);
-        b = gg(b, c, d, a, x[i], 20, -373897302);
-        a = gg(a, b, c, d, x[i + 5], 5, -701558691);
-        d = gg(d, a, b, c, x[i + 10], 9, 38016083);
-        c = gg(c, d, a, b, x[i + 15], 14, -660478335);
-        b = gg(b, c, d, a, x[i + 4], 20, -405537848);
-        a = gg(a, b, c, d, x[i + 9], 5, 568446438);
-        d = gg(d, a, b, c, x[i + 14], 9, -1019803690);
-        c = gg(c, d, a, b, x[i + 3], 14, -187363961);
-        b = gg(b, c, d, a, x[i + 8], 20, 1163531501);
-        a = gg(a, b, c, d, x[i + 13], 5, -1444681467);
-        d = gg(d, a, b, c, x[i + 2], 9, -51403784);
-        c = gg(c, d, a, b, x[i + 7], 14, 1735328473);
-        b = gg(b, c, d, a, x[i + 12], 20, -1926607734);
-        a = hh(a, b, c, d, x[i + 5], 4, -378558);
-        d = hh(d, a, b, c, x[i + 8], 11, -2022574463);
-        c = hh(c, d, a, b, x[i + 11], 16, 1839030562);
-        b = hh(b, c, d, a, x[i + 14], 23, -35309556);
-        a = hh(a, b, c, d, x[i + 1], 4, -1530992060);
-        d = hh(d, a, b, c, x[i + 4], 11, 1272893353);
-        c = hh(c, d, a, b, x[i + 7], 16, -155497632);
-        b = hh(b, c, d, a, x[i + 10], 23, -1094730640);
-        a = hh(a, b, c, d, x[i + 13], 4, 681279174);
-        d = hh(d, a, b, c, x[i], 11, -358537222);
-        c = hh(c, d, a, b, x[i + 3], 16, -722521979);
-        b = hh(b, c, d, a, x[i + 6], 23, 76029189);
-        a = hh(a, b, c, d, x[i + 9], 4, -640364487);
-        d = hh(d, a, b, c, x[i + 12], 11, -421815835);
-        c = hh(c, d, a, b, x[i + 15], 16, 530742520);
-        b = hh(b, c, d, a, x[i + 2], 23, -995338651);
-        a = ii(a, b, c, d, x[i], 6, -198630844);
-        d = ii(d, a, b, c, x[i + 7], 10, 1126891415);
-        c = ii(c, d, a, b, x[i + 14], 15, -1416354905);
-        b = ii(b, c, d, a, x[i + 5], 21, -57434055);
-        a = ii(a, b, c, d, x[i + 12], 6, 1700485571);
-        d = ii(d, a, b, c, x[i + 3], 10, -1894986606);
-        c = ii(c, d, a, b, x[i + 10], 15, -1051523);
-        b = ii(b, c, d, a, x[i + 1], 21, -2054922799);
-        a = ii(a, b, c, d, x[i + 8], 6, 1873313359);
-        d = ii(d, a, b, c, x[i + 15], 10, -30611744);
-        c = ii(c, d, a, b, x[i + 6], 15, -1560198380);
-        b = ii(b, c, d, a, x[i + 13], 21, 1309151649);
-        a = ii(a, b, c, d, x[i + 4], 6, -145523070);
-        d = ii(d, a, b, c, x[i + 11], 10, -1120210379);
-        c = ii(c, d, a, b, x[i + 2], 15, 718787259);
-        b = ii(b, c, d, a, x[i + 9], 21, -343485551);
-        a = ad(a, olda);
-        b = ad(b, oldb);
-        c = ad(c, oldc);
-        d = ad(d, oldd);
-    }
-    return rh(a) + rh(b) + rh(c) + rh(d);
-}
-
-function getCSRFToken() {
-    return md5(Date.now().toString() + "lyedu");
-}
-
-const semester_resp = await fetch("/api/baseInfo/semester/selectCurrentXnXq", {
-    headers: {
-        "accept": "application/json, text/plain, */*",
-        "authorization": "Bearer undefined",
-        "csrftoken": getCSRFToken(),
-    },
-    method: "GET",
-});
-const semester_data = await semester_resp.json();
-
-const user = JSON.parse(('; ' + document.cookie).split(`; user=`).pop().split(';')[0]);
-const user_id = user['userName'];
-
-const num_resp = await fetch(`https://jw.jgsu.edu.cn:19995/api/student/studentInfo/selectXsSyByid/${user_id}`);
-const num_data = await num_resp.json();
-const student_num = num_data['data']['xh'];
-
-const data_resp = await fetch('/api/arrange/CourseScheduleAllQuery/studentCourseSchedule', {
-    headers: {
-        "accept": "application/json",
-        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        "access-control-allow-headers": "Origin, Content-Type, X-Auth-Token",
-        "access-control-allow-methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-        "access-control-allow-origin": "*",
-        "authorization": "Bearer undefined",
-        "content-type": "application/json;charset=UTF-8",
-        "csrftoken": getCSRFToken(),
-        "permission": "undefined",
-        "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"104\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-requested-with": "XMLHttpRequest"
-    },
-    method: "POST",
-    body: JSON.stringify({
-        "oddOrDouble": 0,
-        "semester": user['semester'],
-        "startWeek": "1",
-        "stopWeek": "22",
-        "studentId": user_id,
-        "weeks": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+    const data_resp = await fetch('/api/arrange/CourseScheduleAllQuery/studentCourseSchedule', {
+        headers: {
+            "accept": "application/json",
+            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+            "access-control-allow-headers": "Origin, Content-Type, X-Auth-Token",
+            "access-control-allow-methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+            "access-control-allow-origin": "*",
+            "authorization": "Bearer undefined",
+            "content-type": "application/json;charset=UTF-8",
+            "csrftoken": getCSRFToken(),
+            "permission": "undefined",
+            "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Microsoft Edge\";v=\"104\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "\"Windows\"",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "x-requested-with": "XMLHttpRequest"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "oddOrDouble": 0,
+            "semester": user['semester'],
+            "startWeek": "1",
+            "stopWeek": "22",
+            "studentId": user_id,
+            "weeks": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
+        })
     })
-})
-const data = await data_resp.json();
+    const data = await data_resp.json();
 
-const payload = {}
-payload['student_num'] = student_num;
-payload['semester_name'] = user['semester']
-payload['semester_start_time'] = semester_data['data']['ksrq']
-payload['data'] = data['data'];
+    const payload = {}
+    payload['student_num'] = student_num;
+    payload['semester_name'] = user['semester']
+    payload['semester_start_time'] = semester_data['data']['ksrq']
+    payload['data'] = data['data'];
 
-console.log(JSON.stringify(payload));
+    console.log(JSON.stringify(payload));
+})();
